25e3b893ab68bec4b22b52494487a4367244fc15
Logan Howard <hlogan235@gmail.com>
2022-10-28 20:20:41 -0400

    Functional puppy slideshow. May work out the height problems later + add navigation

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,1 +1,34 @@
-//dab
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next and previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+//thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  //   let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  //   captionText.innerHTML = dots[slideIndex - 1].alt;
+}
